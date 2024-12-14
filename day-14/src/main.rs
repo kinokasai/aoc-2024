4ce4b77aeed9e0a8e38083c48173bfde17412d89
Alexandre Doussot <doussot.alexandre@gmail.com>
2024-12-14 14:14:00 +0100

    day-14 part 2

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -22,7 +22,13 @@ struct Map {
 }
 
 impl Map {
-  fn print(&self, robs_pos: &HashMap<(isize, isize), i32>) {
+  fn print(&self, robs: &Vec<Robot>) {
+    let mut robs_pos = HashMap::<(isize, isize), i32>::new();
+    robs.iter()
+      .for_each(|rob| {
+        let prev = robs_pos.get(&rob.pos).unwrap_or_else(|| &0);
+        robs_pos.insert(rob.pos, prev + 1);
+      });
     for y in 0..self.height {
       for x in 0..self.width {
         match robs_pos.get(&(x, y)) {
@@ -50,32 +56,21 @@ impl Robot {
 }
 
 fn main() {
-  let map = Map { width : 11, height: 7};
   let map = Map { width : 101, height: 103};
   let mut robs = parse("./src/full_input");
-  (0..100).for_each(|_| robs.iter_mut().for_each(|rob| rob.move_(&map)));
-  let mut robs_pos = HashMap::<(isize, isize), i32>::new();
-  robs.iter()
-    .for_each(|rob| {
-      let prev = robs_pos.get(&rob.pos).unwrap_or_else(|| &0);
-      robs_pos.insert(rob.pos, prev + 1);
-  });
-  map.print(&robs_pos);
-  let mut quadrants = [0,0,0,0];
-  for rob in robs.iter() {
-    if map.width % 2 == 1 && rob.pos.0 == map.width / 2 {
-      continue; 
-    } else if map.height % 2 == 1 && rob.pos.1 == map.height / 2 {
-      continue;
+  // map.print(&robs_pos);
+  for i in 0..10000 {
+    let mut counter = 0;
+    for rob in robs.iter_mut() {
+      rob.move_(&map);
+      if rob.pos.0 > map.width / 4 && rob.pos.0 < 3 * map.width / 4
+        && rob.pos.1 > map.height / 4 && rob.pos.1 < 3 * map.height / 4 {
+          counter += 1
+      }
     }
-    match (rob.pos.0 < map.width / 2, rob.pos.1 < map.height / 2) {
-      (true, true) => quadrants[0] += 1,
-      (false, true) => quadrants[1] += 1,
-      (true, false) => {println!("{:?}", rob.pos); quadrants[2] += 1}
-      (false, false) => quadrants[3] += 1,
+    if counter > 300 {
+      println!("iteration {:?}: ", i);
+      map.print(&robs);
     }
   }
-  println!("{:?}", quadrants);
-  let safety_factor = quadrants.into_iter().reduce(|a, i| a * i).unwrap();
-  println!("{}", safety_factor);
 }
